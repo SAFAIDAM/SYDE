b2f5a2aebe41c40b695fdcd878a3de50f8c4b72f
SAFA IDAM HAMED <116453067+SAFAIDAM@users.noreply.github.com>
2023-12-08 14:03:13 +0100

    script commit

--- dist/script.js
+++ dist/script.js
@@ -1,28 +1,28 @@
-let i = 0;
-let images = [];
-let time = 3000;
+// let i = 0;
+// let images = [];
+// let time = 3000;
 
 
-images[0] = "./assets/slidemodel.png";
-images[1] = "./assets/slidemodel2.png";
-images[2] = "./assets/slidemodel3.png";
+// images[0] = "./assets/slidemodel.png";
+// images[1] = "./assets/slidemodel2.png";
+// images[2] = "./assets/slidemodel3.png";
 
-var preloadedImages = [];
-for (var j = 0; j < images.length; j++) {
-  preloadedImages[j] = new Image();
-  preloadedImages[j].src = images[j];
-}
+// var preloadedImages = [];
+// for (var j = 0; j < images.length; j++) {
+//   preloadedImages[j] = new Image();
+//   preloadedImages[j].src = images[j];
+// }
 
-function changeImg() {
-  document.slide.src = images[i];
+// function changeImg() {
+//   document.slide.src = images[i];
 
-  if (i < images.length - 1) {
-    i++;
-  } else {
-    i = 0;
-  }
+//   if (i < images.length - 1) {
+//     i++;
+//   } else {
+//     i = 0;
+//   }
 
-  setTimeout(changeImg, time);
-}
+//   setTimeout(changeImg, time);
+// }
 
-window.onload = changeImg;
+// window.onload = changeImg;
